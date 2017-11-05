aaaa5334ad61770085f9deefdb5010018697d7d0
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-05 08:21:51 -0500

    :white_check_mark: Literal().raw is ""

---
+++ spec/dev/literal.spec.js
@@ -0,0 +1,33 @@
+// conchiolin: Dynamically create static /bin/sh scripts
+// Copyright 2017 Matthew Alexander LaChance
+//
+// This file is part of conchiolin.
+//
+// conchiolin is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// conchiolin is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with conchiolin. If not, see <http://www.gnu.org/licenses/>.
+
+/* eslint-env mocha */
+const expect = require("chai").expect;
+const Literal = require("../../lib/literal");
+
+let literal;
+
+describe("Literal()", () => {
+  beforeEach(() => {
+    literal = Literal();
+  });
+
+  it("is an empty string", () => {
+    expect(literal.raw).to.equal("");
+  });
+});
