aaaa76d7ce0478986b92883088d0aa30780dc5f8
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-23 14:15:48 -0500

    :white_check_mark: Simplify single quote escapes

--- spec/dev/literal.spec.js
+++ spec/dev/literal.spec.js
@@ -75,7 +75,7 @@ describeLiteral("with spaces",
                 '"with spaces"');
 describeLiteral(4, "4", "'4'", '"4"');
 describeLiteral("\\", "\\\\", "'\\'", '"\\\\"');
-describeLiteral("'", "\\'", "''\\'''", '"\'"');
+describeLiteral("'", "\\'", "\\'", '"\'"');
 describeLiteral('"', '\\"', "'\"'", '"\\""');
 describeLiteral("$", "\\$", "'$'", '"\\$"');
 describeLiteral(" !\"#$%&'()*+,-./"
